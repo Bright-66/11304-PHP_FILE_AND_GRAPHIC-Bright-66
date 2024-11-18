cc4f43ac7bd0599f8a49916ac36318fc1c120d4c
sunbody1024 <sunbody1024@gmail.com>
2024-11-18 16:36:22 +0800

    manage 增加控制圖片顯示隱藏的功能 & 合併insert和update 為save()

--- update_img.php
+++ update_img.php
@@ -1,16 +1,19 @@
 <?php
 include_once "function.php";
 
-$imgName=$_POST['imgName'];
-echo $imgName;
-if(isset($_FILES['img'])){
-    if($_FILES['img']['error']==0){
-    
-        move_uploaded_file($_FILES['img']['tmp_name'],"./files/".$imgName);
-        // 更新檔案時 維持原本檔案的檔名!
-    
-    }else{
-        echo "上傳失敗，請檢查檔案格式或是大小是否符合規定";
+$id=$_POST['id'];
+
+$row=find('imgs',$id);
+//dd($_POST);
+dd($row);
+$row['desc']=$_POST['desc'];
+
+if(isset($_FILES['filename'])){
+    if($_FILES['filename']['error']==0){
+        unlink("./files/".$row['filename']);
+        $row['filename']=time() . $_FILES['filename']['name'];
+        move_uploaded_file($_FILES['filename']['tmp_name'],"./files/".$row['filename']);
+        save('imgs',$row);
     }
 }
 
